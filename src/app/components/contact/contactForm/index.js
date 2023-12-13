901ea79e407e53e6aa6e9d1bb37e663fd2e3cae0
Devarapalli Dinesh <92310861+Dineshdevarapalli@users.noreply.github.com>
2023-12-13 22:05:20 +0530

    Update index.js

--- src/app/components/contact/contactForm/index.js
+++ src/app/components/contact/contactForm/index.js
@@ -17,10 +17,7 @@ const ContactForm = ({
         resolver: yupResolver(schema)
     })
     const onSubmit = data => console.log(data)
-    <script
-src='//fw-cdn.com/10985260/3738874.js'
-chat='true'>
-</script>
+ 
 
     const FormBuilder = [
         {
